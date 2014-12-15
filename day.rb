2f48d7994b8ff4b2aed83c3d3355d7bf7b2d5dfe
Nathan Menge <mengen@upmc.edu>
2014-12-14 22:52:34 -0500

    Added date offset to Day class as well as minor refactoring

--- day.rb
+++ day.rb
@@ -1,17 +1,19 @@
-class Day
-  require 'time'
+require 'time'
 
-  attr_reader :time, :distance, :notes
+class Day
+  VALID_PARAMS = [:time, :distance, :notes].freeze
+  DEFAULTS = {start_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 8, 0, 0, "-04:00") }.freeze
+
+  VALID_PARAMS.each { |param| attr_reader param.to_sym }
 
   def initialize(params={})
     values = param_normalization(params)
-    @time = values[:time]
-    @distance = values[:distance]
-    @notes = values[:notes]
+    values.each { |key, value| self.instance_variable_set("@#{key}".to_sym, value) if VALID_PARAMS.include?(key) }
   end
 
   def param_normalization(params)
-    params[:time] ||= Time.now
+    params[:offset] ||= 0
+    params[:time] ||= DEFAULTS[:start_time] + (params[:offset] * 24 * 60 * 60)
     params[:time] = params[:time].utc
     unless params[:distance].is_a?(Numeric) or params[:distance] == 'NA'
       if params.has_key?(:notes)
